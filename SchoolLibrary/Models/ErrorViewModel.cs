8293d34234e81f4457a2e18c9b02b834e1cefe25
WoShiWeilo <wojiaofengren@gmail.com>
2026-09-11 16:24:18 +0400

    Initial ASP.NET Core MVC project

---
+++ SchoolLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SchoolLibrary.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
